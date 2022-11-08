1d4c87b2b175e40ec251439f3d044e382fd9cfe3
MrJotaku <joemelito1193@gmail.com>
2022-11-07 21:46:26 -0500

    Wilks Calc
    - Wilks Calc works
     * There is a bug with the UI but the calculations should be correct *

--- LiftingLog/Views/WilksCalculatorController.swift
+++ LiftingLog/Views/WilksCalculatorController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class WilksCalculatorController: UIViewController {
 
+    var genderValue: String = ""
+    var calcServ = CalculationsService()
     @IBOutlet weak var genderSelector: UIButton!
     @IBOutlet weak var bodyWeightTF: UITextField!
     @IBOutlet weak var maxBenchTF: UITextField!
@@ -21,27 +23,61 @@ class WilksCalculatorController: UIViewController {
         super.viewDidLoad()
 
         title = "Wilks Calculator"
-        setGenderValue()
+      
         self.hideKeyboardWhenTappedAround()
     }
     
-    func setGenderValue(){
+    
+    
+    @IBAction func calculateWilksButton(_ sender: UIButton) {
+        guard let bodyWeight = bodyWeightTF.text, !bodyWeight.isEmpty, let maxBench = maxBenchTF.text, !maxBench.isEmpty,
+              let maxSquat = maxSquatTF.text, !maxSquat.isEmpty, let maxDeadLift = maxDeadLiftTF.text, !maxDeadLift.isEmpty
+        else{
+            showAlert(title: "Blank Values", message: "Bodyweight, Max Bench, Max Squat, & Max Deadlift Cannot Be Left Blank")
+            return
+        }
+        
+        var genderValue = setGenderValue()
+        let doubleBodyWeight = Double(bodyWeightTF.text!) ?? 0.0
+        let doubleMaxBench = Double(maxBenchTF.text!) ?? 0.0
+        let doubleMaxSquat = Double(maxSquatTF.text!) ?? 0.0
+        let doubleMaxDeadLift = Double(maxDeadLiftTF.text!) ?? 0.0
+       
+        if genderValue == "Male"
+        {
+           let wilksOutput = calcServ.calculateMensWilksNumberInPounds(maxBench: doubleMaxBench, maxSquat: doubleMaxSquat, maxDeadlift: doubleMaxDeadLift, bodyWeight: doubleBodyWeight)
+            wilksScoreTF.text = String(wilksOutput)
+        }
+        if genderValue == "Female"
+        {
+            let wilksOutput = calcServ.calculateWomensWilksNumberInPounds(maxBench: doubleMaxBench, maxSquat: doubleMaxSquat, maxDeadlift: doubleMaxDeadLift, bodyWeight: doubleBodyWeight)
+            wilksScoreTF.text = String(wilksOutput)
+        }
+    }
+    
+    func setGenderValue() -> String{
         let maleGender = {(action: UIAction) in
-            // set male selected here
+            self.genderValue = "Male"
             print("Male Selected")
         }
         
         let femaleGender = {(action: UIAction) in
-            // set female selected here
+            self.genderValue = "Female"
             print("Female Selected")
         }
         
+        let selectGender = {(action: UIAction) in
+            self.genderValue = ""
+            print("No Gender Selected")
+        }
         
         genderSelector.menu = UIMenu(children: [
+            UIAction(title: "Tap To Select Gender", state: .off, handler: selectGender),
             UIAction(title: "Male", state: .on, handler: maleGender),
             UIAction(title: "Female", state: .on, handler: femaleGender)])
         
         genderSelector.showsMenuAsPrimaryAction = true
-        genderSelector.changesSelectionAsPrimaryAction = true 
+        genderSelector.changesSelectionAsPrimaryAction = true
+        return genderValue
     }
 }
